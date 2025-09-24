56abda255a3d148c44c01895ba060d0cebe5c6e4
lukastff <lucastavaresffr@gmail.com>
2025-09-24 00:09:00 -0300

    feat: Adicionando ChatList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import ChatList from "./components/ChatList";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="flex h-screen w-screen font-sans bg-[#222323] overflow-hidden">
       <Sidebar />
+      <ChatList />
     </div>
   );
 }
---
+++ src/components/ChatList/index.tsx
@@ -0,0 +1,22 @@
+function ChatList() {
+  return (
+    <div className="border-x-[0.5px] border-[#6d6d6d] text-white p-5 max-w-[565px] w-full h-full">
+      <h1 className="text-[25px] font-bold mb-[20px]">WhatsApp</h1>
+      <input
+        className="w-full h-[40px] px-[20px] bg-white rounded-[50px] mb-[40px] text-black"
+        type="text"
+      />
+      <ul>
+        <li className="flex gap-5">
+          <div className="w-[45px] h-[45px] rounded-[50%] bg-white"></div>
+          <div className="flex flex-col justify-between">
+            <p>Pizza Hut</p>
+            <p className="text-white opacity-50 text-xs">ULTIMA MSG</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default ChatList;
